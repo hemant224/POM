e19e97f7e4460ba2798ddcaec707e68561673fbb
hemantku <hemantku@amdocs.com>
2021-08-08 22:36:59 +0530

    new changge

--- PageObjectModel/src/main/java/com/qa/hubsport/base/BasePage.java
+++ PageObjectModel/src/main/java/com/qa/hubsport/base/BasePage.java
@@ -1,5 +1,100 @@
 package com.qa.hubsport.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+/**
+ * @author hemantkumar
+ *
+ */
+
+
 public class BasePage {
+
+	public WebDriver driver;
+	public Properties prop;
+	
+	/**
+	 * this method is used to initialize browser from given browser
+	 * @param browser
+	 * @return this returns webdriver driver 
+	 */
+
+	public WebDriver init_driver(String browser) {
+
+		if (browser.equalsIgnoreCase("chrome")) {
+
+			System.out.println("browser value is " + browser);
+
+			WebDriverManager.chromedriver().setup();
+
+			driver = new ChromeDriver();
+		}
+
+		else if (browser.equalsIgnoreCase("firefox")) {
+
+			WebDriverManager.firefoxdriver().setup();
+
+			driver = new FirefoxDriver();
+		}
+
+		else if (browser.equalsIgnoreCase("safari")) {
+
+			driver = new SafariDriver();
+		}
+
+		else {
+
+			System.out.println("Enter correct browser " + bbrowser);
+		}
+       
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		return driver;
+
+	}
+	
+	
+	/**
+	 * 
+	 * read properties file like url  and browser
+	 * 
+	 * return prop for properties file
+	 */
+	
+	
+	public Properties init_prop() {
+		
+		try {
+			FileInputStream ip = new FileInputStream("src/main/java/com/qa/hubspot/config/config.properties");
+			try {
+				prop.load(ip);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return prop;
+		
+		
+	}
+	
 
 }
--- PageObjectModel/src/main/java/com/qa/hubspot/pages/LoginPage.java
+++ PageObjectModel/src/main/java/com/qa/hubspot/pages/LoginPage.java
@@ -1,7 +1,29 @@
 package com.qa.hubspot.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 import com.qa.hubsport.base.BasePage;
 
 public class LoginPage extends BasePage {
+
+	private WebDriver driver;
+
+	private By emailId = By.id("username");
+	private By loginBtn = By.xpath("//p[normalize-space()='Login or Create Account']");
+	private By contiuneBtn = By.xpath("//button[@class='capText font16']");
+	private By passwordInput = By.xpath("//input[@id='password']");
+
+	public LoginPage(WebDriver driver) {
+
+		this.driver = driver;
+
+	}
+	
+	
+	public String getLoginPaggeTitile() {
+		
+		return driver.getTitle();
+	}
 
 }
